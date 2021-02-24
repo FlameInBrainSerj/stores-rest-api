ef2188a8aac368992aba23d9bda47c931168f19c
Sergey <Krivosheev.sergey.01@mail.ru>
2021-02-24 03:06:05 +0300

    Solved error

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = "jose"
 api = Api(app)
 
-
+db.init_app(app)
 @app.before_first_request
 def create_tables():
     db.create_all()
@@ -29,6 +29,6 @@ api.add_resource(StoreList, "/stores")
 
 api.add_resource(UserRegister, "/register")
 
-db.init_app(app)
+
 if __name__ == "__main__":     # to prevent running app, when importing anything from this file
     app.run(port=5000, debug=True)
